9c71a4532b1844fad788f0bb73746f372599f60f
PovilasKorop <povilask007@gmail.com>
2021-10-11 07:37:38 +0300

    Before and After

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,7 +8,11 @@ class PostController extends Controller
 {
     public function index()
     {
+        // With N + 1 Query Problem:
         $posts = Post::all();
+
+        // The correct way with Eager loading:
+        // $posts = Post::with('user')->get();
 
         return view('posts.index', compact('posts'));
     }
